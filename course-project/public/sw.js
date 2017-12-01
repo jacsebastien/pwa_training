32deee2e153c624b3b276a735ca4551b7a01d567
jacsebastien <jac.sebastien@gmail.com>
2017-12-01 10:53:19 +0100

    add app.js to cache

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,12 +1,14 @@
 // access to the service workers with "self"
 self.addEventListener('install', (event) => {
     console.log('[Service Worker] Installing Service Workers ...', event);
-    // wait until caches has finished before continuing
+    // wait until caches has finished before continuing because caches is a promise to avoid continuing before it's done
     event.waitUntil(
         // open a new cache if exists or create a new one and name it like we want
         caches.open('static')
         .then((cache) => {
             console.log('[Service Worker] Precaching App Shell');
+            // add path to the files that need to be stored in the cache (relative from web root file)
+            cache.add('/src/js/app.js');
         })
     );
 });
@@ -18,9 +20,24 @@ self.addEventListener('activate', (event) => {
     return self.clients.claim();
 });
 
-// when html page load resources (css/script/images) or when we manually send fetch request from js
+// triggered each time page try to load resources (css/script/images) 
+// or when we manually send fetch request from js
 self.addEventListener('fetch', (event) => {
-    // console.log('[Service Worker] Fetching something ...', event);
     // intercept fetch event and override it
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        // fetch data from cache if available
+        // caches refer to the all chache storage
+        // the key of cache is a request
+        caches.match(event.request) 
+        .then((response) => {
+            // response = null if no cache found
+            if(response) {
+                // get it from cache if exists
+                return response;
+            } else {
+                // if not found, continue with original network request
+                return fetch(event.request);
+            }
+        })
+    );
 });
