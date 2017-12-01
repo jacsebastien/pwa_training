3e3c48635f26f4df0fbbc82909bbd0ad3b44fac6
jacsebastien <jac.sebastien@gmail.com>
2017-12-01 14:13:56 +0100

    add files to cache

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -8,7 +8,22 @@ self.addEventListener('install', (event) => {
         .then((cache) => {
             console.log('[Service Worker] Precaching App Shell');
             // add path to the files that need to be stored in the cache (relative from web root file)
-            cache.add('/src/js/app.js');
+            cache.addAll([
+                '/', // cache request (index is accessible with root path)
+                '/index.html',
+                '/src/js/app.js',
+                '/src/js/feed.js',
+                '/src/js/promise.js',
+                '/src/js/fetch.js',
+                '/src/js/material.min.js',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+            ]); 
+            // cache.add('/index.html');
         })
     );
 });
