bbec59630297d24418bbe1c31b1685bce7113ed7
jacsebastien <jac.sebastien@gmail.com>
2017-12-08 11:13:21 +0100

    add save button

--- course-project/public/src/js/feed.js
+++ course-project/public/src/js/feed.js
@@ -30,10 +30,15 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
+function onSaveButtonClicked(event) {
+    console.log("Clicked");
+}
+
 // create html element with dummmy data
 function createCard() {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+
   var cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
   cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
@@ -46,10 +51,18 @@ function createCard() {
   cardTitleTextElement.className = 'mdl-card__title-text';
   cardTitleTextElement.textContent = 'San Francisco Trip';
   cardTitle.appendChild(cardTitleTextElement);
+
   var cardSupportingText = document.createElement('div');
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+
+  var cardSaveButton = document.createElement('button');
+  cardSaveButton.textContent = "Save";
+  cardSaveButton.addEventListener('click', onSaveButtonClicked);
+
+  cardSupportingText.appendChild(cardSaveButton);
+
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
